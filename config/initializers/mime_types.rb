b5dbd491355f6b4c1cf6c23974f5e07b90e39ecf
joseneto <jlneto@mcfox.com.br>
2025-09-02 16:18:29 -0300

    Atualizando para Rails 8

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,1 +1,9 @@
-Mime::Type.register "application/xls", :xls
+# Ensure custom formats are registered for respond_to in modern Rails
+# Guard against re-registration in host apps
+unless Mime::Type.lookup_by_extension(:csv)
+  Mime::Type.register "text/csv", :csv
+end
+
+unless Mime::Type.lookup_by_extension(:xls)
+  Mime::Type.register "application/vnd.ms-excel", :xls
+end
